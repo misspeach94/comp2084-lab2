4bed29982c8c437de6b74d1336565533720110f9
misspeach94 <sarahcroft@live.ca>
2019-09-20 14:13:11 -0400

    Added Comments

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -12,9 +12,11 @@ namespace lab2.Controllers
         {
             return View();
         }
-
+        //In your controller, add a method called Details that accepts 1 string parameter.
+        //This method sends back a string to the view using the ViewBag that says "You selected category: [paramater-value-here]"
         public IActionResult Details(string categories)
         {
+
             ViewBag.statement = "You selected category: " + categories;
             return View();
         }
